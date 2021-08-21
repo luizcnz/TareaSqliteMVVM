96161c57416c48a4ab069e8088d2ea6243ced712
Luiz <cluisromero@hotmail.com>
2021-08-21 17:22:42 -0600

    Agregar archivos de proyecto.

---
+++ TareaSqliteMVVM/TareaSqliteMVVM/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaSqliteMVVM
+{
+    public partial class App : Application
+    {
+        public static string EmpleadosDB = string.Empty;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string DBlocal)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            EmpleadosDB = DBlocal;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TareaSqliteMVVM/TareaSqliteMVVM/Models/Empleados.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaSqliteMVVM.Models
+{
+    public class Empleados
+    {
+        [PrimaryKey, AutoIncrement]
+        public int idEmpleado { get; set; }
+
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [MaxLength(50)]
+        public string Apellido { get; set; }
+
+
+        public int Edad { get; set; }
+
+        [MaxLength(100)]
+        public string Direccion { get; set; }
+
+        [MaxLength(50)]
+        public string Puesto { get; set; }
+
+
+    }
+}
